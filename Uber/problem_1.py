b805eb0d3978d05901c51a143256be7fa75c0626
HouariZEGAI <houarizegai14@gmail.com>
2019-03-02 16:46:36 +0100

    Uber - Solved problem #1, but complexity O(n^2) :D

--- Uber/problem_1.py
+++ Uber/problem_1.py
@@ -7,3 +7,14 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 Follow-up: what if you can't use division?
 
 """
+
+in_array =  [3, 2, 1]
+out_array = []
+for i in range(len(in_array)):
+	res = 1
+	for j in range(len(in_array)):
+		if j != i:
+			res *= in_array[j]
+	out_array.append(res)
+
+print(out_array)
