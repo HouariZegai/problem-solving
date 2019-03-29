9ebc65205ca4d9928a1a3cbca0e1021013aa053d
HouariZEGAI <houarizegai14@gmail.com>
2019-03-29 22:23:17 +0100

    Microsoft - Adding problem #2 [Hard]

---
+++ Microsoft/problem_2.py
@@ -0,0 +1,7 @@
+""" Asked by: Microsoft [Hard].
+
+You have an N by N board. 
+Write a function that, given N, returns the number of possible arrangements of the board where N queens can be placed on the board 
+without threatening each other, i.e. no two queens share the same row, column, or diagonal.
+
+"""
