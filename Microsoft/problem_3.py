7693e0dac9312877038c65a842d91b6cd5dab6a3
HouariZEGAI <houarizegai14@gmail.com>
2019-04-11 23:54:03 +0100

    Microsoft - Adding problem #3 [Easy]

---
+++ Microsoft/problem_3.py
@@ -0,0 +1,18 @@
+""" Asked by: Microsoft [Easy].
+
+Suppose an arithmetic expression is given as a binary tree. 
+Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
+
+Given the root to such a tree, write a function to evaluate it.
+
+For example, given the following tree:
+
+    *
+   / \
+  +    +
+ / \  / \
+3  2  4  5
+
+You should return 45, as it is (3 + 2) * (4 + 5).
+
+"""
