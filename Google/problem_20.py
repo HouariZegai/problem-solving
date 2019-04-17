a889cab469a39a936b0aca32e53b7ee27dd24d0f
HouariZEGAI <houarizegai14@gmail.com>
2019-04-17 22:31:01 +0100

    Google - Adding problem #20 [Medium]

---
+++ Google/problem_20.py
@@ -0,0 +1,7 @@
+""" Asked by: Google [Medium].
+
+Given an undirected graph represented as an adjacency matrix and an integer k, 
+write a function to determine whether each vertex in the graph can be colored such that no two adjacent 
+vertices share the same color using at most k colors.
+
+"""
