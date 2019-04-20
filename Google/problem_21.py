02a79e77626a45d807814bc434e1faba970d8268
HouariZEGAI <houarizegai14@gmail.com>
2019-04-20 20:33:20 +0100

    Google - Adding problem #21 [Hard]

---
+++ Google/problem_21.py
@@ -0,0 +1,6 @@
+""" Asked by: Google [Hard].
+
+Implement a file syncing algorithm for two computers over a low-bandwidth network.
+What if we know the files in the two computers are mostly the same?
+
+"""
