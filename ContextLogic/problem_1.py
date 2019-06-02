e5a7ed29bd6b21fb521aae1316d1310e726ec6d6
unknown <houarizegai14@gmail.com>
2019-06-02 18:30:52 +0100

    ContextLogic - adding problem #1 [MEDIUM]

---
+++ ContextLogic/problem_1.py
@@ -0,0 +1,6 @@
+""" Asked by: ContextLogic [Medium].
+
+Implement division of two positive integers without using the division, multiplication, or modulus operators. 
+Return the quotient as an integer, ignoring the remainder.
+
+"""
