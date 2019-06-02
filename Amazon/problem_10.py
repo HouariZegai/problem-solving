00e01da6305893f335866453c2aa2b28434fc54c
unknown <houarizegai14@gmail.com>
2019-06-02 18:24:48 +0100

    Amazon - adding problem #10 [Medium]

---
+++ Amazon/problem_10.py
@@ -0,0 +1,6 @@
+""" Asked by: Amazon [Medium]
+
+Given a matrix of 1s and 0s, return the number of "islands" in the matrix. 
+A 1 represents land and 0 represents water, so an island is a group of 1s that are neighboring whose perimeter is surrounded by water.
+
+"""
