3db139be591a4d1bbc6a347d2b0daf329e5ddcc8
unknown <houarizegai14@gmail.com>
2019-06-02 18:21:57 +0100

    Microsoft - adding problem #8 [Easy]

---
+++ Microsoft/problem_8.py
@@ -0,0 +1,7 @@
+""" Asked by: Microsoft [Easy].
+
+Using a read7() method that returns 7 characters from a file, implement readN(n) which reads n characters.
+
+For example, given a file with the content “Hello world”, three read7() returns “Hello w”, “orld” and then “”.
+
+"""
