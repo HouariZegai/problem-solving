e5c0f687c25ab53356dabe36d11911cd25b70405
unknown <houarizegai14@gmail.com>
2019-06-03 18:29:33 +0100

    Microsoft - adding problem #9 [Easy]

---
+++ Microsoft/problem_9.py
@@ -0,0 +1,7 @@
+""" Asked by: Microsoft [Easy].
+
+Given a number in the form of a list of digits, return all possible permutations.
+
+For example, given [1,2,3], return [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]].
+
+"""
