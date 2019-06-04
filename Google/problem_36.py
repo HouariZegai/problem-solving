08247c64da9b86f79705d1d9147efdd8115fdc71
unknown <houarizegai14@gmail.com>
2019-06-04 18:59:07 +0100

    Google - adding problem #36 (about linkedlist palindrome) [Easy]

---
+++ Google/problem_36.py
@@ -0,0 +1,7 @@
+""" Asked by: Google [Easy].
+
+Determine whether a doubly linked list is a palindrome. What if it’s singly linked?
+
+For example, 1 -> 4 -> 3 -> 4 -> 1 returns True while 1 -> 4 returns False.
+
+"""
