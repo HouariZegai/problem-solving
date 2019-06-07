5d0e73f0e00e1365911e94615b32d331835b4561
HouariZegai <houarizegai14@gmail.com>
2019-06-07 17:39:32 +0100

    Pinterest - adding problem #1

---
+++ Pinterest/problem_1.py
@@ -0,0 +1,8 @@
+""" Asked by: Pinterest [Medium].
+
+Given an integer list where each number represents the number of hops you can make, 
+determine whether you can reach to the last index starting at index 0.
+
+For example, [2, 0, 1, 0] returns True while [1, 1, 0, 1] returns False.
+
+"""
