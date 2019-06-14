83129bd80a4dc8d26d098a532ede0d950217393d
HouariZegai <houarizegai14@gmail.com>
2019-06-14 20:09:28 +0100

    Facebook - adding problem #16 [Medium]

---
+++ Facebook/problem_16.py
@@ -0,0 +1,15 @@
+""" Asked by: Facebook [Medium].
+
+Given a binary tree, return all paths from the root to leaves.
+
+For example, given the tree:
+
+   1
+  / \
+ 2   3
+    / \
+   4   5
+
+Return [[1, 2], [1, 3, 4], [1, 3, 5]].
+
+"""
