ccb25e14de8e388b25440e08b9b1147220676e32
HouariZegai <houarizegai14@gmail.com>
2019-06-14 20:13:57 +0100

    Google - adding problem #39 [Medium]

---
+++ Google/problem_39.py
@@ -0,0 +1,7 @@
+""" Asked by: Google [Medium].
+
+Given a string of words delimited by spaces, reverse the words in string. For example, given "hello world here", return "here world hello"
+
+Follow-up: given a mutable string representation, can you perform this operation in-place?
+
+"""
