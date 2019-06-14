482ca4e73d9ae54e2a3787a19a330f43b173446d
HouariZegai <houarizegai14@gmail.com>
2019-06-14 20:11:12 +0100

    Google - adding problem #38 [Hard]

---
+++ Google/problem_38.py
@@ -0,0 +1,7 @@
+""" Asked by: Google [Hard].
+
+Given a word W and a string S, find all starting indices in S which are anagrams of W.
+
+For example, given that W is "ab", and S is "abxaba", return 0, 3, and 4.
+
+"""
