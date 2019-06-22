25ec5f423d2f506729862e1d6f9cf789aa132208
HouariZegai <houarizegai14@gmail.com>
2019-06-22 22:36:25 +0100

    Google - Adding problem #43 [Hard]

---
+++ Google/problem_43.py
@@ -0,0 +1,7 @@
+""" Asked by: Google [Hard].
+
+Given a string which we can delete at most k, return whether you can make a palindrome.
+
+For example, given 'waterrfetawx' and a k of 2, you could delete f and x to get 'waterretaw'.
+
+"""
