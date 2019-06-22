baa9c2aa21a8b7daa4019da11130740de57b7022
HouariZegai <houarizegai14@gmail.com>
2019-06-22 22:35:07 +0100

    Microsoft - Adding problem #42 [Medium] (singleton)

---
+++ Microsoft/problem_12.py
@@ -0,0 +1,7 @@
+""" Asked by: Microsoft [Medium].
+
+Implement the singleton pattern with a twist. First, instead of storing one instance, store two instances. 
+And in every even call of getInstance(), return the first instance and in every odd call of getInstance(), 
+return the second instance.
+
+"""
