e89972a60f0cea989bd3e57ad07ed188f14e9374
HouariZegai <houarizegai14@gmail.com>
2019-06-22 22:28:36 +0100

    Jane Street - Adding problem #3

---
+++ JaneStreet/problem_3.py
@@ -0,0 +1,7 @@
+""" Asked by: Jane Street [Medium].
+
+Generate a finite, but an arbitrarily large binary tree quickly in O(1).
+
+That is, generate() should return a tree whose size is unbounded but finite.
+
+"""
