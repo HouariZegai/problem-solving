885e9cc7c29b0fecbb219b25ff517647c7c38de9
HouariZegai <houarizegai14@gmail.com>
2019-06-29 12:50:10 +0100

    Google - Adding problem #44 [Easy]

---
+++ Google/problem_44.py
@@ -0,0 +1,7 @@
+""" Asked by: Google [Easy].
+
+Given the root of a binary search tree, and a target K, return two nodes in the tree whose sum equals K.
+
+For example, given the following tree and K of 20
+
+"""
