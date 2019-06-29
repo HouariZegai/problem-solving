303a6cc9bfdfe33a0795a9926865975411e0fead
HouariZegai <houarizegai14@gmail.com>
2019-06-29 12:51:55 +0100

    Facebook - Adding problem #19 [Medium]

---
+++ Facebook/problem_19.py
@@ -0,0 +1,8 @@
+""" Asked by: Facebook [Medium].
+
+Write a function that rotates a list by k elements. 
+For example, [1, 2, 3, 4, 5, 6] rotated by two becomes [3, 4, 5, 6, 1, 2]. 
+Try solving this without creating a copy of the list. 
+How many swap or move operations do you need?
+
+"""
