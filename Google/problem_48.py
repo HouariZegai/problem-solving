0951b0dc27e87c8d098ae8326f428a7706c97d25
HouariZegai <houarizegai14@gmail.com>
2019-07-11 22:32:51 +0100

    Google - adding problem #48 [Hard]

---
+++ Google/problem_48.py
@@ -0,0 +1,8 @@
+""" Asked by: Google [Hard].
+
+You're given a string consisting solely of (, ), and *. * can represent either a (, ), 
+or an empty string. Determine whether the parentheses are balanced.
+
+For example, (()* and (*) are balanced. )*( is not balanced.
+
+"""
