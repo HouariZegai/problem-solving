cb62f40330c2f3f01d9e3c50189744920c93259e
HouariZegai <houarizegai14@gmail.com>
2019-07-24 15:42:28 +0100

    Goldman Sachs - adding problem #1 [Hard]

---
+++ GoldmanSachs/problem_1.py
@@ -0,0 +1,11 @@
+""" Asked by: GoldmanSachs [Hard].
+
+Given a list of numbers L, implement a method sum(i, j) which returns the sum from the sublist L[i:j] (including i, excluding j).
+
+For example, given L = [1, 2, 3, 4, 5], sum(1, 3) should return sum([2, 3]), which is 5.
+
+You can assume that you can do some pre-processing. sum() should be optimized over the pre-processing step.
+
+Ready to interview? Take Triplebyte's quiz and skip straight to final interviews with top tech companies!
+
+"""
