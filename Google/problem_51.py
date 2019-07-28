12dae8e1af68675d1827eab70fe920c32d078a9a
HouariZegai <houarizegai14@gmail.com>
2019-07-28 18:27:38 +0100

    Google - adding problem #51 [Easy]

---
+++ Google/problem_51.py
@@ -0,0 +1,7 @@
+""" Asked by: Google [Easy].
+
+Given a string, return the first recurring character in it, or null if there is no recurring character.
+
+For example, given the string "acbbac", return "b". Given the string "abcdef", return null.
+
+"""
