20e6a71a040363dde60c7dcfeff05c3b4b77ac5f
HouariZegai <houarizegai14@gmail.com>
2019-08-06 18:17:24 +0100

    Square - adding problem #3 [Medium]

---
+++ Square/problem_3.py
@@ -0,0 +1,18 @@
+""" Asked by: Square [Medium].
+
+Given a list of words, return the shortest unique prefix of each word. For example, given the list:
+
+dog
+cat
+apple
+apricot
+fish
+Return the list:
+
+d
+c
+app
+apr
+f
+
+"""
