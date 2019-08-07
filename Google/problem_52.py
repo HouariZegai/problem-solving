bb249783855a948570a5291bf5f9e92f7cde1ec6
HouariZegai <houarizegai14@gmail.com>
2019-08-07 18:27:35 +0100

    Google - adding problem #52 [Medium]

---
+++ Google/problem_52.py
@@ -0,0 +1,6 @@
+""" Asked by: Google [Medium].
+
+You are given an array of length n + 1 whose elements belong to the set {1, 2, ..., n}. 
+By the pigeonhole principle, there must be a duplicate. Find it in linear time and space.
+
+"""
