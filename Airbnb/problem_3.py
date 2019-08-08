ca9abecbf2cb592d10e07d5ee54ad3c27ac5cee9
HouariZegai <houarizegai14@gmail.com>
2019-08-08 18:31:42 +0100

    Airbnb - adding problem #3 [Hard]

---
+++ Airbnb/problem_3.py
@@ -0,0 +1,7 @@
+""" Asked by: Airbnb [Hard].
+
+Given a list of words, find all pairs of unique indices such that the concatenation of the two words is a palindrome.
+
+For example, given the list ["code", "edoc", "da", "d"], return [(0, 1), (1, 0), (2, 3)].
+
+"""
