fbb2c31825f76c946fbc4fbe6e97b5271c30760d
HouariZegai <houarizegai14@gmail.com>
2019-08-13 12:04:33 +0100

    Microsoft - adding problem #16 [Medium]

---
+++ Microsoft/problem_16.py
@@ -0,0 +1,9 @@
+""" Asked by: Microsoft [Medium].
+
+Describe and give an example of each of the following types of polymorphism:
+
+Ad-hoc polymorphism
+Parametric polymorphism
+Subtype polymorphism
+
+"""
