aa912d210b0f7840a9690c680df7e0e955b02c38
HouariZegai <houarizegai14@gmail.com>
2020-02-05 22:14:19 +0100

    Microsoft - adding problem #18 [Hard]

---
+++ Microsoft/problem_18.py
@@ -0,0 +1,7 @@
+""" Asked by: Microsoft [Hard].
+
+Let X be a set of n intervals on the real line. We say that a set of points P "stabs" X if every interval in X contains at least one point in P. Compute the smallest set of points that stabs X.
+
+For example, given the intervals [(1, 4), (4, 5), (7, 9), (9, 12)], you should return [4, 9].
+
+"""
