5698ecb79c68efef50b7502105544cca0df75888
HouariZegai <houarizegai14@gmail.com>
2020-02-05 22:13:06 +0100

    Facebook - adding problem #30 [Hard]

---
+++ Facebook/problem_30.py
@@ -0,0 +1,7 @@
+""" Asked by: Facebook [Hard].
+
+Given a string of parentheses, find the balanced string that can be produced from it using the minimum number of insertions and deletions. If there are multiple solutions, return any of them.
+
+For example, given "(()", you could return "(())". Given "))()(", you could return "()()()()".
+
+"""
