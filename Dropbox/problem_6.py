676a6d404709165e604bd93b0a5b05660251d4d8
HouariZegai <houarizegai14@gmail.com>
2020-02-09 22:31:03 +0100

    Adding problem #6 [Medium] by Dropbox

---
+++ Dropbox/problem_6.py
@@ -0,0 +1,7 @@
+""" Asked by: Dropbox [Medium].
+
+Given an undirected graph G, check whether it is bipartite. 
+Recall that a graph is bipartite if its vertices can be divided into two independent sets, U and V, 
+such that no edge connects vertices of the same set.
+
+"""
