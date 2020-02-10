bb82c26c15cbb8b5d820d9425a777a30b38a17cf
HouariZegai <houarizegai14@gmail.com>
2020-02-10 22:33:39 +0100

    Adding problem #1 [Hard] by YouTube

---
+++ YouTube/problem_1.py
@@ -0,0 +1,7 @@
+""" Asked by: YouTube [Hard]
+
+Write a program that computes the length of the longest common subsequence of three given strings. 
+For example, given "epidemiologist", "refrigeration", and "supercalifragilisticexpialodocious", 
+it should return 5, since the longest common subsequence is "eieio".
+
+"""
