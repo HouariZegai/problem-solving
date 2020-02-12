0f41c521d7ff9352383060497374396dc9d7dc57
HouariZegai <houarizegai14@gmail.com>
2020-02-12 22:43:50 +0100

    Adding problem #5 [Medium] by Stripe

---
+++ Stripe/problem_5.py
@@ -0,0 +1,7 @@
+""" Asked by: Stripe [Easy].
+
+Given an integer n, return the length of the longest consecutive run of 1s in its binary representation.
+
+For example, given 156, you should return 3.
+
+"""
