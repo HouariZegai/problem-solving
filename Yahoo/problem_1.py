b21e2dbfb0f4bef2841a6ff63f24a5573fb7365f
HouariZegai <houarizegai14@gmail.com>
2020-02-16 22:49:16 +0100

    Adding problem #1 [Medium] by Yahoo

---
+++ Yahoo/problem_1.py
@@ -0,0 +1,6 @@
+""" Asked by: Yahoo [Medium]
+
+Write an algorithm that computes the reversal of a directed graph. 
+For example, if a graph consists of A -> B -> C, it should become A <- B <- C.
+
+***
