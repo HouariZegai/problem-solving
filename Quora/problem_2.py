ef88416a846c59b19f882066b226ce574326a8e8
HouariZegai <houarizegai14@gmail.com>
2020-02-18 22:53:39 +0100

    Adding problem #2 [Medium] by Quora

---
+++ Quora/problem_2.py
@@ -0,0 +1,7 @@
+""" Asked by: Quora [Medium].
+
+Given an absolute pathname that may have . or .. as part of it, return the shortest standardized path.
+
+For example, given "/usr/bin/../bin/./scripts/../", return "/usr/bin/".
+
+"""
