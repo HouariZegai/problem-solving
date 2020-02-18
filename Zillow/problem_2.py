68327cf10bfdaf09700dad06fad1126c3211b7a7
HouariZegai <houarizegai14@gmail.com>
2020-02-18 22:52:43 +0100

    Adding problem #2 [Easy] by Zillow

---
+++ Zillow/problem_2.py
@@ -0,0 +1,7 @@
+""" Asked by: Zillow [Easy]
+
+Let's define a "sevenish" number to be one which is either a power of 7, or the sum of unique powers of 7. 
+The first few sevenish numbers are 1, 7, 8, 49, and so on. 
+Create an algorithm to find the nth sevenish number.
+
+"""
