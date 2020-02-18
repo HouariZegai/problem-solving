b0a74a9b81a35933676a3db72d08c2e4eb3e8462
HouariZegai <houarizegai14@gmail.com>
2020-02-18 22:55:05 +0100

    Adding problem #4 [Hard] by Palantir

---
+++ Palantir/problem_4.py
@@ -0,0 +1,7 @@
+""" Asked by: Palantir [Hard].
+
+Typically, an implementation of in-order traversal of a binary tree has O(h) space complexity, 
+where h is the height of the tree. 
+Write a program to compute the in-order traversal of a binary tree using O(1) space.
+
+"""
