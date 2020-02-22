c018ea24dbd2c578fc90f3369fbe2f80d1861488
HouariZegai <houarizegai14@gmail.com>
2020-02-22 11:12:25 +0100

    Adding problem #5 by Twitter [Medium]

---
+++ Twitter/problem_5.py
@@ -0,0 +1,6 @@
+""" Asked by: Twitter [Medium].
+
+Given a list of numbers, create an algorithm that arranges them in order to form the largest possible integer. 
+For example, given [10, 7, 76, 415], you should return 77641510.
+
+"""
