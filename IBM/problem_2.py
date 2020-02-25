5faa6e2884b628f71af46000d26f1491b3e3c4e0
HouariZegai <houarizegai14@gmail.com>
2020-02-25 13:00:26 +0100

    Adding problem #2 by IBM [Easy]

---
+++ IBM/problem_2.py
@@ -0,0 +1,7 @@
+""" Asked by: IBM [Easy].
+
+Given a string with repeated characters, rearrange the string so that no two adjacent characters are the same. If this is not possible, return None.
+
+For example, given "aaabbc", you could return "ababac". Given "aaab", return None.
+
+"""
