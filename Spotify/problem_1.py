350669cc207b426ad7cbeb8143f977222422a959
HouariZegai <houarizegai14@gmail.com>
2020-02-26 13:12:34 +0100

    Adding problem #1 by Spotify [Hard]

---
+++ Spotify/problem_1.py
@@ -0,0 +1,8 @@
+""" Asked by: Spotify [Hard].
+
+There are N couples sitting in a row of length 2 * N. 
+They are currently ordered randomly, but would like to rearrange themselves so that each couple's partners can sit side by side.
+
+What is the minimum number of swaps necessary for this to happen?
+
+"""
