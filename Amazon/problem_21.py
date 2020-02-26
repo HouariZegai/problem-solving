f6e767f4a47c41b6d57028774ba49813b485388c
HouariZegai <houarizegai14@gmail.com>
2020-02-26 13:08:52 +0100

    Adding problem #21 by Amazon [Easy]

---
+++ Amazon/problem_21.py
@@ -0,0 +1,12 @@
+""" Asked by: Amazon [Easy]
+
+A tree is symmetric if its data and shape remain unchanged when it is reflected about the root node. The following tree is an example:
+
+        4
+      / | \
+    3   5   3
+  /           \
+9              9
+Given a k-ary tree, determine whether it is symmetric.
+
+"""
